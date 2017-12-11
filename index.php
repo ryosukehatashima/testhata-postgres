9ae2db471cc8f34e921fdcd86781f360fc503439
caoyangit <caoyan0728@gmail.com>
2017-12-11 17:50:34 +0900

    test

--- index.php
+++ index.php
@@ -12,9 +12,13 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
+$tbl_name = "postgres_test"
+
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+  $psql foo -c "create table t1 (a int, b int);"
+  $psql foo -c "insert into t1(a, b) values(1,1),(2,2);"
 } else {
   print "Tables in your database:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
