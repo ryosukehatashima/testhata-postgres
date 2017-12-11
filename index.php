9277a5b985db69945d0af880e4c7d9478b8811f6
caoyangit <caoyan0728@gmail.com>
2017-12-11 20:02:52 +0900

    test

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $db = new \PDO($type.':host='.$url['host'].($url['port'] ? ';port='.$url['port']
 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
-//$conn = pg_pconnect("dbname=d4agtqbk4gjt8r");
-$conn = pg_pconnect("dbname=substr($url['path'], 1)");
+$conn = pg_pconnect("dbname=postgres://tesffrvqsjjitg:f7fe5cb78a0e3f8613dd069fce19324160d3d225b47853f80e05dafa0589fc9c@ec2-107-20-214-99.compute-1.amazonaws.com:5432/d4agtqbk4gjt8r");
+//$conn = pg_pconnect("dbname=substr($url['path'], 1)");
 if (!$conn) {
   echo "An error occurred1.\n";
   exit;
