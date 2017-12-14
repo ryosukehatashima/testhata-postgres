60a910b8aed52fcf56de575b6d96af13f7ef1c45
caoyangit <caoyan0728@gmail.com>
2017-12-14 20:37:15 +0900

    test

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $sql = file_get_contents('install/'.$type.'.sql');
 $db = new \PDO($type.':host='.$url['host'].($url['port'] ? ';port='.$url['port'] : '').';dbname='.substr($url['path'], 1), $url['user'], $url['pass'], $options);
 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+echo '<p>cp2</p>';
 
 $db->exec($sql);
 ?>
